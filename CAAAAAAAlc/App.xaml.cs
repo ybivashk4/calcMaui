94b2b24f2e5b1c06f015b09025070abe1edc653b
mihail B <beltyukov-mihail@mail.ru>
2024-10-01 14:59:00 +0500

    Добавьте файлы проекта.

---
+++ CAAAAAAAlc/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CAAAAAAAlc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CAAAAAAAlc/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CAAAAAAAlc
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
